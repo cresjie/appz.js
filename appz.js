b6874974d76f2b3648f531ac005b75f6bffc97c8
Cres Jie <cresjie@gmail.com>
2018-02-19 17:49:15 +0800

    initial commit

---
+++ appz.js
@@ -0,0 +1,64 @@
+(function(){
+	window.appz = function(){};
+
+	/**
+	 * storage helper, instead of using window.localStorage object
+	 */
+	var storageData = {};
+	appz.storage = {
+		set: function(id, val){ return storageData[id] = val; },
+		get: function(id){ storageData.hasOwnProperty(id) ? storageData[id] : undefined; },
+		remove: function(id){ return delete storageData[id]; },
+		clear: function(){ return storageData = {}; }
+	};
+
+	function _Promise() {
+		Promise.apply(this, arguments);
+	}
+	/**
+	 * wrapper of jQuery.ajax
+	 * this function will automatically retry the query
+	 * before calling the callbacks
+	 */
+	appz.ajax = function(settings, counter){
+		/*
+		 * by default, 3 retries
+		 */
+		counter = counter || 3;
+		var tried = 1;
+		
+		var promise = new Promise(function(resolve, rejected){
+
+			var doQuery = function() {
+				
+				 $.ajax(settings).always(function(data, textStatus, jqXHR){
+
+					if(textStatus == 'success') {
+						resolve(data);
+					} else {
+						/**
+						 * retry the query if its less than the query
+						 */
+						if( tried < counter) {
+							tried++;
+							doQuery();
+						} else {
+							
+							rejected(data);
+							
+							
+						}
+					}
+				});
+				
+			};
+
+			doQuery();
+			
+		});
+
+		promise.catch(function(){});
+
+		return promise;
+	}
+})();
